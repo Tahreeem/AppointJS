3b9083fe527805ec2b5e200df981eb5c0c3477ca
Tahreem <tahreemsohailbutt@gmail.com>
2019-04-17 00:59:39 -0400

    debugging for heroku

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ mongoose.connect(process.env.REACT_APP_MONGODB_URI
   || "mongodb://localhost/bookings",
   { useNewUrlParser: true, useCreateIndex: true, } );
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
   
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
